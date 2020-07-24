8681c63ffd462fc5c44020a0deffc22aff091317
Sean Garfola <sagarfola@gmail.com>
2020-07-23 21:00:09 -0400

    Git Commit Chapter 7 Complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tcod
+
 import copy
+
+import color
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
@@ -11,7 +14,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -36,6 +39,10 @@ def main() -> None:
     )
     engine.update_fov()
 
+    engine.message_log.add_message(
+        "Hello and welcome to yet another dungeon!", color.welcome_text
+    )
+
     # This function initializes and creates the screen
     with tcod.context.new_terminal(
         screen_width,
@@ -49,9 +56,11 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         # This is the game loop!
         while True:
-            engine.render(console=root_console, context=context)
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
 
-            engine.event_handler.handle_events()
+            engine.event_handler.handle_events(context)
 
 
 # When you run the script, it will run everything based on the
